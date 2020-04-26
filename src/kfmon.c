9409b2c49ae7acdb9f329e662438c88ce5e69fce
NiLuJe <ninuje@gmail.com>
2020-04-26 04:43:20 +0200

    Comment typo

--- src/kfmon.c
+++ src/kfmon.c
@@ -178,9 +178,9 @@ int nm_kfmon_simple_request(const char *ipc_cmd, const char *ipc_arg) {
     }
 
     // We'll be polling the socket for a reply, this'll make things neater, and allows us to abort on timeout,
-    // in the unlickely event there's already an IPC session being handled by KFMon,
+    // in the unlikely event there's already an IPC session being handled by KFMon,
     // in which case the reply would be delayed by an undeterminate amount of time (i.e., until KFMon gets to it).
-    // Timeout after 2s
+    // Here, we'll want to timeout after 2s
     failed = wait_for_reply(data_fd, 500, 4);
     // NOTE: We happen to be done with the connection right now.
     //       But if we still needed it, KFMON_IPC_POLL_FAILURE would warrant an early abort w/ a forced close().
